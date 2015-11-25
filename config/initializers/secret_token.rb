fb31aa42e7ace61a8c0bfd9a69dcbdf1e0857334
adevenuto <anthonydevenuto@gmail.com>
2015-11-25 22:26:18 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urtake::Application.config.secret_key_base = 'f1b8648f3191f7caa57460fb9b5146119a132b1eb26e6066e8ed98ba74b6559cee334378df9c5524ff42a841d7288495cff4210e47d1fb03c57f831963377e8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Urtake::Application.config.session_store :cookie_store, key: '_urtake_session'
