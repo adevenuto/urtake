9565757946a2168caf5af9075fa602203926d8a0
adevenuto <anthonydevenuto@gmail.com>
2015-11-29 01:03:06 +0000

    added gallery look to 900+ screen size

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all.page(params[:page]).per(3)
+    @places = Place.all.page(params[:page]).per(6)
   end
   def new
     @place = Place.new
