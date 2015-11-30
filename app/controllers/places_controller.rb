008959b263188ec2c1bef2fc152058ccbaa5db24
adevenuto <anthonydevenuto@gmail.com>
2015-11-30 01:57:17 +0000

    connected post to specific user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,14 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
   def index
-    @places = Place.all.page(params[:page]).per(6)
+    @places = Place.all
+
   end
   def new
     @place = Place.new
   end
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
