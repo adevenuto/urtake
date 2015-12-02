ea2ac6d11149b4cea1c8c01ac2bf160ae57f0ec6
adevenuto <anthonydevenuto@gmail.com>
2015-12-02 02:20:20 +0000

    added validation to other field/name min => 3

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ActiveRecord::Base
   # include Elasticsearch::Model::Callbacks
 
   belongs_to :user
-  validates :name, :presence => true
+  validates :name, :presence => true,  length: { minimum: 3 }
   validates :address, :presence => true
   validates :description, :presence => true
 end
