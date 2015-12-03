eccf87bbab1d44494c7aabb91b8d88027f98aeea
adevenuto <anthonydevenuto@gmail.com>
2015-12-03 04:43:26 +0000

    made user comments save to the database comment/user_id/place_id

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
   def index
-    @places = Place.order('created_at DESC')
-    # .page(params[:page]).per(8)
+    @places = Place.order('created_at DESC').page(params[:page]).per(8)
     # @places = Place.search((params[:q].present? ? params[:q] : '*')).records
   end
   def new
