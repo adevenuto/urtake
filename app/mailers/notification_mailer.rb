d47c72af7593c24c9b25d3947d5e8d707bd608ac
adevenuto <anthonydevenuto@gmail.com>
2015-12-07 18:05:55 +0000

    basic email sending

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@urtakeapp.com"
 
   def comment_added
-    mail(to: "anthonydevenuto@gmail.com",
-      subject: "A comment has been added to your place")
+    mail(to: "urtakeadev@gmail.com",
+      subject: " A comment has been added to your place")
   end
 end
